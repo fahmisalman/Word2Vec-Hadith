a4495ceb52d22be92b238b5a6b2f06d911ec6fc8
fahmisalman <fahmisalman@gmail.com>
2019-02-12 12:03:52 +0700

    Add train & testing

--- Data-Proc.py
+++ Data-Proc.py
@@ -15,19 +15,19 @@ def load_data():
 
 if __name__ == '__main__':
 
-    # x = load_data()
-    # x_prep = []
-    # for i, row in enumerate(x):
-    #     x_prep.append(simple_preprocess(row))
-    # model = Word2Vec(
-    #     x_prep,
-    #     size=1500,
-    #     window=10,
-    #     min_count=2,
-    #     workers=10)
-    # model.train(x_prep, total_examples=len(x_prep), epochs=10)
-    # model.save('hadith_size=1500_window=10.model')
+    x = load_data()
+    x_prep = []
+    for i, row in enumerate(x):
+        x_prep.append(simple_preprocess(row))
+    model = Word2Vec(
+        x_prep,
+        size=1500,
+        window=10,
+        min_count=2,
+        workers=10)
+    model.train(x_prep, total_examples=len(x_prep), epochs=10)
+    model.save('hadith_size=1500_window=10.model')
     model = Word2Vec.load('hadith_size=1500_window=10.model')
-    w = 'nabi'
+    w = 'allah'
     print(model.wv.most_similar(positive=w))
 
